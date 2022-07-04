5e1b1b108f9c127cf44c55835e116c798a890562
rainsen <yusen@rainsen.comcd>
2022-07-04 11:31:18 +0800

    first commit

---
+++ tests/DinglogTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Rainsens\Dinglog\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TinglogTest extends TestCase
+{
+	/** @test */
+	public function it_returns_a_ramdom_text()
+	{
+		$this->assertTrue(true);
+	}
+}
